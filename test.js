eab034c19c80497fe03512bcaf6a38e15355b97b
Ryan <chimon2000@users.noreply.github.com>
2016-03-17 15:18:44 -0400

    Linting fix.

--- test.js
+++ test.js
@@ -45,7 +45,7 @@ test('ignore directories with flag "--nodir"', async t => {
 	fs.mkdirSync(t.context.tmp);
 	fs.mkdirSync(path.join(t.context.tmp, 'nodir'));
 	fs.writeFileSync(path.join(t.context.tmp, 'nodir/hello.js'), 'console.log("hello");');
-	
+
 	await execa('./cli.js', [path.join(t.context.tmp, 'nodir/**/*'), path.join(t.context.tmp, 'dest'), '--nodir']);
 	t.is(read(t.context.tmp, 'nodir/hello.js'), read(t.context.tmp, 'dest/hello.js'));
 });
